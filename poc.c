7593071bb3b78130abf025cf80f6b355ac471183
yuvi2605 <35652235+yuvi2605@users.noreply.github.com>
2018-01-23 16:31:01 +0530

    Create poc.c

---
+++ poc.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter the number");
+    scanf("%d",&a);
+    if(a>0)
+    printf("positive");
+    else if(a<0)
+    printf("negative");
+    else
+    printf("zero");
+
+}
