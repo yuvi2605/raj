e41334424d3d89059606ac07c1c25417dbca97e9
yuvi2605 <35652235+yuvi2605@users.noreply.github.com>
2018-01-23 16:57:51 +0530

    Create alp.c

---
+++ alp.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+char a;
+printf("\n enter a character:");
+scanf("%c",&a);
+if((a=='a')&&(a=='z')||((a=='A')&&(a=='Z')))
+print("\n it is an alphabet");
+else
+printf("\n it is not an alphabet");
+getch();
+}
